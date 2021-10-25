79e198cf9c0c7d0200487b50d4aa168672e7c2c5
Divij Jain <jaindivijwork@gmail.com>
2021-10-25 22:57:32 +0530

    Initial Stage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes.js";
 import Footer from "./Footer";
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map((note) => {
+                return (
+                    <Note
+                        key={note.id}
+                        heading={note.title}
+                        desc={note.content}
+                    />
+                );
+            })}
             <Footer />
         </div>
     );
